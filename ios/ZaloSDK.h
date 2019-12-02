f1d695172d0fbf42dc2ac4f24ab83139d66b303c
tiendd <tiendd@mobiletech.vn>
2019-12-02 08:49:36 +0700

    .

---
+++ ios/ZaloSDK.h
@@ -0,0 +1,22 @@
+//
+//  ZaloSDKLite.h
+//  ZaloSDK
+//
+//  Created by Liem Vo Uy on 4/28/14.
+//  Copyright (c) 2014 VNG. All rights reserved.
+//
+
+
+#import "ZOResponseObject.h"
+#import "ZOZaloError.h"
+#import "ZaloSDKDefine.h"
+#import "ZOOauthResponseObject.h"
+#import "ZORequestPermissionResponseObject.h"
+#import "ZOZaloApiDelegate.h"
+#import "ZOGraphResponseObject.h"
+#import "ZOZingMeLoginView.h"
+#import "ZOLoginViewController.h"
+#import "ZOTokenResponseObject.h"
+#import "ZDK.h"
+#import "ZOFeed.h"
+#import "ZDKConfig.h"
