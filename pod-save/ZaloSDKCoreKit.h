16c67f5fa02b637968bfe57fe6b2d70686f285e8
tiendd <tiendd@mobiletech.vn>
2020-01-07 11:09:51 +0700

    .

---
+++ pod-save/ZaloSDKCoreKit.h
@@ -0,0 +1,44 @@
+//
+//  ZaloSDKCoreKit.h
+//  ZaloSDKCoreKit
+//
+//  Created by Hoang Nguyen on 9/29/15.
+//  Copyright © 2015 VNG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZaloSDKCoreKit.
+FOUNDATION_EXPORT double ZaloSDKCoreKitVersionNumber;
+
+//! Project version string for ZaloSDKCoreKit.
+FOUNDATION_EXPORT const unsigned char ZaloSDKCoreKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import "./PublicHeader.h>
+
+
+#import "./ZDKLogManager.h"
+#import "./NSData+ZDKExtension.h"
+#import "./NSDictionary+ZDKExtension.h"
+#import "./NSMutableDictionary+ZDKExtension.h"
+#import "./NSMutableArray+ZDKExtension.h"
+#import "./NSString+ZDKExtension.h"
+#import "./UIDevice+ZDKExtension.h"
+#import "./UIImage+ZDKExtension.h"
+#import "./UITextField+ZDKExtension.h"
+#import "./UIView+ZDKAutoLayout.h"
+#import "./UIViewController+ZDKExtension.h"
+#import "./ZOReachability.h"
+#import "./ZOReachability+SharedInstance.h"
+#import "./ZDKProgressHUD.h"
+#import "./ZDKMacros.h"
+#import "./ZDKUtility.h"
+#import "./ZDKResponse.h"
+#import "./ZDKConstants.h"
+#import "./ZDKApplication.h"
+#import "./ZDKApplicationDelegate.h"
+#import "./ZDKSharedStorage.h"
+#import "./ZDKDeviceTracker.h"
+#import "./ZDKNetworkManager.h"
+#import "./ZDKSettingManager.h"
+
